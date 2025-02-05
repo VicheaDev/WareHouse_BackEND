0eaf1697d201f1ab10239c6cfb921cbf7ab35cd2
Vichea69 <smart.vichea@gmail.com>
2025-02-05 21:31:29 +0700

    Add dockerfile

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use('/api/v1/categories', categoryRoutes);
 app.use('/api/v1/products', productRoutes);
 app.get('/', (req, res) => {
-    res.send('Welcome to the APP API');
+    res.send('Welcome to the Server API');
 });
 
 module.exports = app;
